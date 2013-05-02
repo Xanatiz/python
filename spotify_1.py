746127907e66795cfa0684bf90309a23c412587c
Xanatiz <henrik.thoreson@gmail.com>
2013-05-02 20:16:32 +0200

    Zipf's sonf done

--- spotify_1.py
+++ spotify_1.py
@@ -1,10 +1,8 @@
 def reverse(string):
-
     if not string:
         return ''
     else:
         return reverse(string[1:])+string[0]
 
 def main(inputInt):
-    binary=bin(inputInt)[2:]
-    print(int(reverse(binary), 2))
+    return (int(reverse(bin(inputInt)[2:]), 2))
